f9455d8dc45d037fa704510be7b6d8fcf8a1dc32
Gary Smith <garysmith193358@gmail.com>
2023-03-15 15:00:11 -0400

    reverted back server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
 import db
-import os
 
 app = Flask(__name__, static_url_path='', static_folder='client/build')
 cors = CORS(app)
@@ -12,7 +11,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def serve():
     print('running')
-    return render_template(os.path.join(app.static_folder, 'index.html'))
+    return send_from_directory(app.static_folder,'index.html')
 
 @app.route("/data")
 @cross_origin()
